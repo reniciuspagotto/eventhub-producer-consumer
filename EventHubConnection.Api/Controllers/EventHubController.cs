3ebe6e94c9f37582dd455ced4346a78990ed21a5
Renícius Pagotto Fostaini <renicius.pagotto@outlook.com>
2021-03-30 13:54:42 -0300

    first commit

---
+++ EventHubConnection.Api/Controllers/EventHubController.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventHubConnection.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventHubController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            const string eventHubConnectionString = "<your event hub connection string>";
+            const string eventHubName = "<your event hub name>";
+
+            await using (var producerClient = new EventHubProducerClient(eventHubConnectionString, eventHubName))
+            {
+                // Create a batch of events 
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                var obj = new
+                {
+                    Product = "Chocolate",
+                    Price = 10
+                };
+
+                // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(obj))));
+
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published.");
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ EventHubConnection.Api/HostedService/ConsumerEventHubHostedService.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventHubConnection.Api.HostedService
+{
+    public class ConsumerEventHubHostedService : BackgroundService
+    {
+        private const string ehubNamespaceConnectionString = "<your event hub connection string>";
+        private const string eventHubName = "<your event hub name>";
+        private const string blobStorageConnectionString = "<your blob connection string>";
+        private const string blobContainerName = "<your blob container name>";
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            // Create a blob container client that the event processor will use 
+            BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+            // Create an event processor client to process events in the event hub
+            EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, ehubNamespaceConnectionString, eventHubName);
+
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            await processor.StartProcessingAsync();
+        }
+
+        public static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tReceived event: {0}", JsonSerializer.Deserialize<object>(eventArgs.Data.Body.ToArray()));
+
+            throw new Exception();
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            //await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        public static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
